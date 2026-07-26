aa6c47896ff80388be99c172a48cd75e2cfc8255
Mateo <mateoglez2005@gmail.com>
2026-07-26 17:53:38 +0200

    Modales instalaciones

--- js/funciones.js
+++ js/funciones.js
@@ -12,4 +12,29 @@ $(document).ready(function(){ //espero a que se cargue el documento entero
 
     });
 
+    //funcion parar el modal de instlaciones
+    function abrirModalInstalaciones(){
+       const modal = bootstrap.Modal.getOrCreateInstance(document.getElementById("modal-instalaciones"));
+       modal.show();
+    }
+
+    //mostrar cada instalacion
+    $("#viviendas").click(function(){
+        abrirModalInstalaciones();
+        $("#titulo-instalaciones").text("Instalaciones Residenciales");
+    });
+
+    $("#industriales").on("click",function(){
+        abrirModalInstalaciones();
+        $("#titulo-instalaciones").text("Intalaciones Industriales");
+    });
+
+    $("#cargadores").on("click",function(){
+        abrirModalInstalaciones();
+        $("#titulo-instalaciones").text("Cargadores Eléctricos");
+    });
+
+
+
 });
+
